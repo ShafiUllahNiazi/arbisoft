81329752edb434c8ab6e13fe27d6fee64f53bd0e
Shafi Ullah <shafi@Shafiullahkhan-Desktop-50.arbisoft.com>
2018-07-27 15:37:19 +0500

    Task has been completed

--- Test.py
+++ Test.py
@@ -1,54 +1,63 @@
-import argparse
+import sys
 
-
-class myclass:
-    """filename = "/home/shafi/Desktop/s/sowpods.txt"
-    file = open(filename, "r")
-    sowpodsList = []
-    for line in file:
-        x = line.strip('\n')
-        sowpodsList.append(x)
-    print(sowpodsList)
-
-    filename = "/home/shafi/Desktop/s/myCopysowpods.txt"
-    file = open(filename, "w")
-    for line in sowpodsList:
-        file.write(line + "\n")
-    file.close()
+"""
+Calculates the scrabble score of valid words
 """
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('rock', help='help', type=str)
+def scrabbleScore(word):
+    scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+              "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+              "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+              "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+              "x": 8, "z": 10}
+    totalScore = 0
+    for score in word:
+        totalScore = totalScore + scores[score]
+    return totalScore
+"""
+Removes the letter from the rack letters to handle repeat letters
+"""
+def my_pop(char, rack):
+    charPlace = rack.find(char)
+    return rack[0:charPlace] + rack[charPlace + 1:]
 
-try:
-    args = parser.parse_args()
-    s = str(args)
-    s = s.lower()
-    print(s)
-except:
-    print("handle this invalid argument ")
-    exit()
+"""
+This function check the word that if it is made of the letters that are subset of rack letters
+"""
+def lettersInRack(word, rack):
+    rack = str(rack)
+    rack = rack.lower()
+    for everyLetter in word:
+        if everyLetter not in rack:
+            return False
+        else:
+            rack = my_pop(everyLetter, rack)
+    return True
 
-
-def find_valid_words():
+"""
+This function gives the valid words that are  made of the letters that are subset of rack letters
+"""
+def find_valid_words(rack_letters):
     filename = "/home/shafi/Desktop/s/sowpods.txt"
     file = open(filename, "r")
-    sowpodsList = []
+    validWords = {}
+    i = 0
     for line in file:
         word = line.strip('\n')
         word = word.lower()
-        sowpodsList.append(word)
-        for everyLetter in word:
-            if everyLetter in s:
-                print(s + " " + word)
-                continue
-        else:break
+        rack = rack_letters
+        if lettersInRack(word, rack):
+            score = scrabbleScore(word)
+            validWords[word] = score
+    print(validWords)
+    print(sorted(validWords))
 
-find_valid_words()
-    #print(sowpodsList)
 """
-everyLetter = "hel"
-if everyLetter in "hello":
-    print(everyLetter)
+This piece of code checks the command line arguents and proceeds for valid words
 """
+if sys.argv.__len__() < 3:
+    find_valid_words(sys.argv[1])
+else:
+    print("please enter the command line argument")
+    exit()
